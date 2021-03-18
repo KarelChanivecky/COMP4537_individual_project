1491f7e519cb2cbfe6ec481394cfbfa4344d780c
karelchanivecky <karl_chanivecky@hotmail.com>
2021-03-18 00:27:23 -0700

    changed base url

--- sharedSymbols/server_info.mjs
+++ sharedSymbols/server_info.mjs
@@ -1,3 +1,4 @@
 const PORT = 10000;
-const BASE_URL = "https://karelc.com/assignment/api";
+const BASE_URL = "https://karelc.com/COMP4537_individual_project/api";
+// const BASE_URL = "localhost";
 export {BASE_URL, PORT};
